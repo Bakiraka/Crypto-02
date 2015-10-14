e5266b008badc7617cc3f171c5ba6ee9e64ff61f
blanquart julien <blanquart@pcmt10.m2fil.univ-lille1.fr>
2015-10-14 10:55:00 +0200

    ajout et verification d'un hash de verif de non vol

--- createCheck.sh
+++ createCheck.sh
@@ -11,7 +11,7 @@ clientPKHash=`cat clientPkEncode`
 echo clientPkHash : $clientPKHash
 commercantPkEncode=`openssl dgst -sha1 commercantPk | cut -d' ' -f2 | openssl rsautl -decrypt -raw -inkey clientSk | base64`
 echo commercantPkEncode : $commercantPkEncode
-resultxor=$(( montant ^ uid ))
-resultxorcrypt=`echo $resultxor | openssl rsautl -decrypt -raw -inkey clientSk | base64 `
+resultxor="$montant $uid $commercantPkEncode"
+resultxorcrypt=`echo $resultxor | openssl dgst -sha1 | openssl rsautl -decrypt -raw -inkey clientSk | base64 `
 echo resultxorcrypt : $resultxorcrypt
 echo "$commercantPkEncode\n$clientPKHash\n$uidcrypt\n$montantcrypt\n$resultxorcrypt" > $2
