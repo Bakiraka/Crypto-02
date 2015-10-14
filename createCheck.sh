3e495455476a1457458940c69e7818b69e774794
blanquart julien <blanquart@pcmt10.m2fil.univ-lille1.fr>
2015-10-14 16:32:56 +0200

    correction ?

--- createCheck.sh
+++ createCheck.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 Facture=$1
 fichierout=$2
+marqueur="0000000000000"
 uid=`cat $Facture | head -1`
 montant=`cat $Facture | tail -1`
 uidcrypt=`echo $uid | openssl rsautl -decrypt -inkey clientSk -raw | base64`
@@ -14,4 +15,4 @@ echo commercantPkEncode : $commercantPkEncode
 resultxor="$montant $uid $factureTest"
 resultxorcrypt=`echo $resultxor | openssl dgst -sha1 | openssl rsautl -decrypt -raw -inkey clientSk | base64 `
 echo resultxorcrypt : $resultxorcrypt
-echo "$commercantPkEncode\n\n$clientPKHash\n\n$uidcrypt\n\n$montantcrypt\n\n$resultxorcrypt" > $2
+echo "$commercantPkEncode\n$marqueur\n$clientPKHash\n$marqueur\n$uidcrypt\n$marqueur\n$montantcrypt\n$marqueur\n$resultxorcrypt" > $2
