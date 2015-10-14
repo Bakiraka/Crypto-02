85eda4bab875a479759bdad29f31f390a3e7af71
Bakiraka <anthony.djenane@gmail.com>
2015-10-14 09:03:00 +0200

    rename verif1.sh et complété génération basique chèque

--- merchant_generate_invoice.sh
+++ merchant_generate_invoice.sh
@@ -6,11 +6,9 @@ then
 	echo "No amount for the invoice entered !"
 	exit 0
 fi
-
 nameofinvoice=$1
 amount=$2
 
-echo $nameofinvoice
-echo $amount
-#Génération
+#Génération de l'id unique
 tr -cd '[:digit:]' < /dev/urandom | fold -w30 | head -n1 > $nameofinvoice
+echo $amount >> $nameofinvoice
